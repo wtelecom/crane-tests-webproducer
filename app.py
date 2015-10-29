418fd46e7c06d53b1f53df918bb775872171201a
Jose Fuentes Castillo <jsfuentescastillo@gmail.com>
2015-10-30 00:34:41 +0100

    small changes

--- app.py
+++ app.py
@@ -5,17 +5,12 @@ import redis
 app = Flask(__name__)
 r = redis.StrictRedis(host='db', port=6379, db=0)
 
-@app.route("/")
-def produce():
-    num = r.get('num')
-    if num == None:
-        num = 1
-    else:
-        num = int(num)
-        num += 1
 
-    r.set('num', str(num))
-    return str(num)
+@app.route("/set/<smthng>")
+def produce(number):
+    r.set('data', str(smthng))
+    return {'data':str(smthng), 'action':'set'}
+
 
 if __name__ == "__main__":
         port = int(os.environ.get("PORT", 5001))
