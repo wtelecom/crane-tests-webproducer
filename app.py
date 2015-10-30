fcf3f5d0d663565e6dcbf6e0aa5aa60bc24e5d65
Jose Fuentes Castillo <jsfuentescastillo@gmail.com>
2015-10-30 12:34:32 +0100

    ___

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 import redis
 
 app = Flask(__name__)
@@ -7,9 +7,12 @@ r = redis.StrictRedis(host='db', port=6379, db=0)
 
 
 @app.route("/set/<smthng>")
-def produce(number):
-    r.set('data', str(smthng))
-    return {'data':str(smthng), 'action':'set'}
+def produce(smthng):
+    try:
+        r.set('data', str(smthng))
+    except Exception, e:
+        return str(e)
+    return jsonify({'data':str(smthng), 'action':'set'})
 
 
 if __name__ == "__main__":
